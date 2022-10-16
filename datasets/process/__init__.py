2522f805146b6314c62d971b68846c3ec28bd6c4
이종민 <eel8@naver.com>
2022-10-17 00:03:04 +0900

    revise datasets/__init.py

--- datasets/process/__init__.py
+++ datasets/process/__init__.py
@@ -4,7 +4,7 @@ from .affine_transform import get_affine_transform, exec_affine_transform
 
 from .pose_process import fliplr_joints, half_body_transform
 
-from .heatmaps_process import get_max_preds, get_final_preds, generate_heatmaps, generate_heatmaps_origin
+from .heatmaps_process import get_max_preds, get_final_preds, generate_heatmaps
 
 from .data_format import convert_data_to_annorect_struct
 # from .structure import *
